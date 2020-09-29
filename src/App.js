7c9be4fc162ce36296849c682577773cb3ff80e0
Mohamad <mohamad7291m@gmail.com>
2020-09-29 11:47:33 +0330

    =using styled components instead of makeStyle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBar from "./Components/Navbar";
+import {StylesProvider} from "@material-ui/core/styles";
 
 function App() {
     return (
         <div className="App">
-            <NavBar/>
+            <StylesProvider injectFirst>
+                <NavBar/>
+            </StylesProvider>
         </div>
     );
 }
+
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,20 @@
-import React  from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tab from '@material-ui/core/Tab';
 import TabContext from '@material-ui/lab/TabContext';
 import TabList from '@material-ui/lab/TabList';
+import styled from 'styled-components';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
-    },
-}));
+const StyledAppBar = styled(AppBar)`
+        background : #222;
+    `;
+
+const Div = styled('div')`
+        flex-grow: 1;
+`
 
 export default function NavBar() {
 
-    const classes = useStyles();
     const [value, setValue] = React.useState('1');
 
     const handleChange = (event, newValue) => {
@@ -22,16 +22,16 @@ export default function NavBar() {
     };
 
     return (
-        <div className={classes.root}>
+        <Div>
             <TabContext value={value}>
-                <AppBar style={{backgroundColor:"#222"}} position="static">
+                <StyledAppBar position="static">
                     <TabList onChange={handleChange}>
-                        <Tab label="Users" value="1" />
-                        <Tab label="Products" value="2" />
-                        <Tab label="Manufacturers" value="3" />
+                        <Tab label="Users" value="1"/>
+                        <Tab label="Products" value="2"/>
+                        <Tab label="Manufacturers" value="3"/>
                     </TabList>
-                </AppBar>
+                </StyledAppBar>
             </TabContext>
-        </div>
+        </Div>
     );
 }
