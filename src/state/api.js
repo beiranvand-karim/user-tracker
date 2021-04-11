b46c8f7ea90c4429f421bb6cfe0057d123eb5a48
mohamadp91 <mohamad7291m@gmail.com>
2021-04-11 17:23:05 +0430

    Create an .env file to manage environment variables

--- src/state/api.js
+++ src/state/api.js
@@ -1,11 +1,12 @@
 import axios from "axios"
+import env from "@beam-australia/react-env"
 
 export const sendRequestAdduser = async (user) => {
 	try {
 		const request = {
 			/* eslint-disable */
 			method:"post",
-			url:"http://localhost:8082/api/users",
+			url:`${env("API_HOST")}/api/users`,
 			data: {
 				"firstName":user.firstName,
 				"lastName":user.lastName,
